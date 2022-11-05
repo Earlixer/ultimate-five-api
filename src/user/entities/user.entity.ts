a333cd36fda13e7691161d41ca2b674dcb78ab94
Earl Groenewald <earl.groenewald@standardbank.co.za>
2022-11-06 00:29:16 +0200

    DataBase tables setup

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    Id: number;
+
+    @Column()
+    Name: string;
+
+    @Column()
+    Password: string;
+
+    @Column()
+    Email: string;
+}
+
